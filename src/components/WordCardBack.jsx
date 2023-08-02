cfb99e0327da7caa175b5a28ed77dd4b51e525b8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-08-02 14:44:24 +0300

    create word card front side

---
+++ src/components/WordCardBack.jsx
@@ -0,0 +1,65 @@
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon } from '@ionic/react'
+import { megaphoneOutline } from 'ionicons/icons'
+import { React } from 'react'
+
+const WordCardBack = ({ word }) => {
+	const UK = new Audio(word.header.audio.UK)
+	const US = new Audio(word.header.audio.US)
+
+	/**
+	 *
+	 * @param {string} word
+	 * @param {string} example
+	 */
+
+	return (
+		<>
+			<IonCard className="auth-card">
+				<IonCardTitle
+					className="ion-padding"
+					style={{
+						width: '100%',
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center'
+					}}
+				>
+					Box: {word.box}
+					<div className="ion-floating-right">
+						<IonButton onClick={() => UK.play()}>
+							UK
+							<IonIcon icon={megaphoneOutline}></IonIcon>
+						</IonButton>
+						<IonButton onClick={() => US.play()}>
+							US
+							<IonIcon icon={megaphoneOutline}></IonIcon>
+						</IonButton>
+					</div>
+				</IonCardTitle>
+
+				<IonCardHeader className="ion-text-center">
+					<IonCardTitle>
+						{word.word}
+						<IonCardTitle>
+							({word.header.partOfSpeech}) {word.header.CEFRLevel}
+						</IonCardTitle>
+					</IonCardTitle>
+				</IonCardHeader>
+				<IonCardContent className="ion-floating-left">{word.definitions[0].examples[0]}</IonCardContent>
+			</IonCard>
+			<div
+				style={{
+					width: '100%',
+					display: 'flex',
+					justifyContent: 'space-evenly',
+					alignItems: 'center'
+				}}
+			>
+				<IonButton color={'danger'}>Cancel</IonButton>
+				<IonButton color={'success'}>OK</IonButton>
+			</div>
+		</>
+	)
+}
+
+export default WordCardBack
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,96 @@
-import { IonCard, IonCol, IonContent, IonGrid, IonPage, IonRow } from '@ionic/react'
+import { IonCol, IonContent, IonGrid, IonPage, IonRow } from '@ionic/react'
+import { React } from 'react'
+import WordCardFront from '../components/WordCardFront'
 
 export const Home = () => {
+	const word = {
+		id: 6,
+		box: 0,
+		word: 'about',
+		header: {
+			audio: {
+				UK: 'https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/a/abo/about/about__gb_1.mp3',
+				US: 'https://www.oxfordlearnersdictionaries.com/media/english/us_pron/a/abo/about/about__us_1.mp3'
+			},
+			partOfSpeech: 'adverb',
+			CEFRLevel: 'A1'
+		},
+		definitions: [
+			{
+				meaning: 'a little more or less than; a little before or after',
+				examples: ['It costs about $10.', 'They waited (for) about an hour.', 'He arrived (at) about ten.']
+			},
+			{
+				meaning: 'nearly; very close to',
+				examples: ["I'm just about ready.", 'This is about the best we can hope for.']
+			},
+			{
+				meaning: 'in many directions; here and there',
+				examples: ['The children were rushing about in the garden.']
+			},
+			{
+				meaning: 'in no particular order; in various places',
+				examples: ['Her books were lying about on the floor.']
+			},
+			{
+				meaning: 'doing nothing in particular',
+				examples: ['People were standing about in the road.']
+			},
+			{
+				meaning: 'able to be found in a place',
+				examples: ['There was nobody about.', "There's a lot of flu about.", 'She’s somewhere about—I saw her a few minutes ago.']
+			},
+			{
+				meaning: 'facing the opposite direction',
+				examples: ['He brought the ship about.']
+			}
+		],
+		idioms: [
+			{
+				usage: 'just about (informal)',
+				definition: [
+					{
+						meaning: 'almost; very nearly',
+						examples: ["I've met just about everyone.", '‘Did you reach your sales target?’ ‘Just about.’']
+					},
+					{
+						meaning: 'approximately',
+						examples: ['She should be arriving just about now.']
+					}
+				]
+			},
+			{
+				usage: 'out and about ',
+				definition: [
+					{
+						meaning: 'able to go outside again after an illness',
+						examples: null
+					},
+					{
+						meaning: 'travelling around a place',
+						examples: ["We've been out and about talking to people all over the country."]
+					}
+				]
+			},
+			{
+				usage: 'that’s about all | that’s about it ',
+				definition: [
+					{
+						meaning: 'used to say that you have finished telling somebody about something and there is nothing to add',
+						examples: ["‘Anything else?’ ‘No, that's about it for now.’"]
+					}
+				]
+			}
+		]
+	}
+
 	return (
 		<IonPage>
 			<IonContent color={'primary'} scrollY={false}>
 				<IonGrid className="ion-align-items-center ion-justify-content-center ion-height ">
 					<IonRow className="ion-align-items-center ion-justify-content-center ion-height">
 						<IonCol size="8" size-md="6" size-lg="4">
-							<IonCard>Home Page</IonCard>
+							<WordCardFront word={word} />
 						</IonCol>
 					</IonRow>
 				</IonGrid>
